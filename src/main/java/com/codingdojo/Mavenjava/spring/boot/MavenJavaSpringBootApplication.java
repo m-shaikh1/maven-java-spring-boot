723dbfaa9edd15899eea7ec036d2651f2176f833
m-shaikh1 <mohammed.y.shaikh1@gmail.com>
2022-08-28 14:41:12 +0300

    5

--- src/main/java/com/codingdojo/Mavenjava/spring/boot/MavenJavaSpringBootApplication.java
+++ src/main/java/com/codingdojo/Mavenjava/spring/boot/MavenJavaSpringBootApplication.java
@@ -1,14 +1,23 @@
 package com.codingdojo.Mavenjava.spring.boot;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MavenJavaSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MavenJavaSpringBootApplication.class, args);
-		System.out.println("Hellow World from Mohammed Shaikh!");
+		ApplicationContext ctx = SpringApplication.run(MavenJavaSpringBootApplication.class, args);
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 
 }
