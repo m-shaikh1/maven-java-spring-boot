47bba8d077de1754c28cd22222b9ea0692094a2a
m-shaikh1 <mohammed.y.shaikh1@gmail.com>
2022-08-28 14:49:04 +0300

    6

--- src/main/java/com/codingdojo/Mavenjava/spring/boot/MavenJavaSpringBootApplication.java
+++ src/main/java/com/codingdojo/Mavenjava/spring/boot/MavenJavaSpringBootApplication.java
@@ -2,22 +2,32 @@ package com.codingdojo.Mavenjava.spring.boot;
 
 import java.util.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MavenJavaSpringBootApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(MavenJavaSpringBootApplication.class, args);
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
+		SpringApplication.run(MavenJavaSpringBootApplication.class, args);
+	}
 
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
